05f4d41cc82587b00f23ca9d0c1c9fe6583c8f44
lizkavalski <lizkavalski@gmail.com>
2020-09-14 21:36:26 -0700

    math is still off

--- cart.js
+++ cart.js
@@ -2,22 +2,27 @@
 
 const data = require('./data/store.json');
 
-let cart = [] ;
 
 class Cart {
+  constructor(){
+    this.cart = [];
+  }
+
   addProduct(item, id){
     let getOne = data.filter(data => data.item === item && data.id === id);
-    cart.push(getOne);
-    cart.flat(1);
-    cart = cart.flat();
+    this.cart.push(getOne);
+    this.cart.flat(1);
+    this.cart = this.cart.flat();
   }
+
   removeProduct(item, id){
-    cart = cart.filter(cart => cart.item !== item && cart.id !== id);
+    this.cart = this.cart.filter(cart => cart.item !== item && cart.id !== id);
   }
+
   getAllCart(){
     let map = {};
     let result = [];
-    cart.forEach(function(a){
+    this.cart.forEach(function(a){
       if(!map[a.id]){
         map[a.id] = {
           'item' : a.item,
@@ -26,62 +31,91 @@ class Cart {
           'catagory':a.catagory,
           'taxType':a.taxType,
           'count' : 0,
-        },
+        };
         result.push(map[a.id]);
       }
       map[a.id].count++;
     });
-    cart = result;
-    console.log('This your cart:', cart);
+    this.cart = result;
+    console.log('This your cart:', this.cart);
   }
+
   removeAllCart(){
     // cart.length = 0;
     console.log('Cart is empty');
   }
 
-};
-
-
-class Receipt{
   totalOfEverythingAndSaleTax(){
     let total = 0;
     let totalsalestax = 0;
-    for (const item of cart) {
+    for (const item of this.cart) {
+
       if (item.taxType === 'except' && item.count <= 1 ){
         total += item.price;
         console.log( `${item.item}: $${item.price}` );
       }
       if (item.taxType === 'except' && item.count > 1){
         total += item.price * item.count;
-        console.log( `${item.item}: $${total} (@ $${item.price})` );
+        console.log( `${item.item}: $${total} (${item.count} @ $${item.price})` );
       }
+
       if(item.taxType === 'basic' && item.count <= 1){
-        total += item.price + (item.price * .10);
-        totalsalestax += item.price * .10;
         let saleTax = item.price * .10;
         let itemPlusTax = item.price + saleTax;
-        console.log( `${item.item}: $${itemPlusTax.toFixed(2)} ` );
+        totalsalestax += saleTax;
+        total += itemPlusTax;
+        console.log( `${item.item}: $${itemPlusTax.toFixed(2)}` );
       }
-      if(item.taxType === 'import' && item.catagory === 'Food'){
-        total += item.price + (item.price * .05);
-        totalsalestax += item.price * .05;
+      if (item.taxType === 'basic' && item.count > 1){
+        let saleTax = (item.price * .10) * item.count;
+        let itemsPlusTax = (item.price * item.count) + saleTax;
+        let oneItemPlus = item.price + (item.price * .10);
+        totalsalestax += saleTax;
+        total += itemsPlusTax;
+        console.log( `${item.item}: $${itemsPlusTax.toFixed(2)} (${item.count} @ $${oneItemPlus.toFixed(2)})` );
+      }
+
+
+      if(item.taxType === 'import' && item.catagory === 'Food' && item.count <= 1){
         let saleTax = item.price * .05;
         let itemPlusTax = item.price + saleTax;
-        console.log( `${item.item}: $${Math.ceil(itemPlusTax.toFixed(2) * 100) / 100} ` );
+        totalsalestax += saleTax;
+        total += itemPlusTax;
+        console.log( `${item.item}: $${itemPlusTax.toFixed(2)}` );;
       }
-      if(item.taxType === 'import' && item.catagory !== 'Food'){
-        totalsalestax += item.price * .15;
-        total += item.price + (item.price * .15);
+      if (item.taxType === 'import' && item.catagory === 'Food' && item.count > 1){
+        let saleTax = (item.price * .05) * item.count;
+        let itemsPlusTax = (item.price * item.count) + saleTax;
+        let oneItemPlus = item.price + (item.price * .05);
+        totalsalestax += saleTax;
+        total += itemsPlusTax;
+        console.log( `${item.item}: $${itemsPlusTax.toFixed(2)} (${item.count} @ $${oneItemPlus.toFixed(2)})`);
+      }
+
+
+
+      if(item.taxType === 'import' && item.catagory !== 'Food' && item.count <= 1 ){
         let saleTax = item.price * .15;
         let itemPlusTax = item.price + saleTax;
-        console.log( `${item.item}: $${Math.ceil(itemPlusTax.toFixed(2) * 100) / 100}` );
+        totalsalestax += saleTax;
+        total += itemPlusTax ;
+        console.log( `${item.item}: $${Math.ceil(itemPlusTax * 100) / 100}` );
       }
+      if (item.taxType === 'import' && item.catagory !== 'Food' && item.count > 1){
+        let saleTax = (item.price * .05) * item.count;
+        let itemsPlusTax = (item.price * item.count) + saleTax;
+        let oneItemPlus = item.price + (item.price * .05);
+        totalsalestax += saleTax;
+        total += itemsPlusTax;
+        console.log( `${item.item}: $${itemsPlusTax.toFixed(2)} (${item.count} @ $${oneItemPlus.toFixed(2)})` );
+      }
+
+
     }
-    console.log(`Sales Tax: $${Math.ceil(totalsalestax.toFixed(2) * 100) / 100}`);
-    console.log(`Total: $${total.toFixed(2)}`);
+    console.log(`Sales Tax: $${Math.ceil(totalsalestax * 20) / 20}`);
+    console.log(`Total: $${Math.ceil(total * 100) / 100}`);
   };
 
 };
 
-
-module.exports = {Cart:Cart, Receipt:Receipt,};
+module.exports = {Cart:Cart,};
