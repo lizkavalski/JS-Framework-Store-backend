684a7ce563d281c4ba081e957c8bc9de3742ab85
lizkavalski <lizkavalski@gmail.com>
2021-07-12 09:05:46 -0700

    small fixes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JS-Framework-Store
+[![https://github.com/lizkavalski/JS-Framework-Store-backend/actions](https://github.com/lizkavalski/JS-Framework-Store-backend/actions/workflows/node.js.yml/badge.svg)](https://github.com/lizkavalski/codeChallenges-backend/actions)
 
 A backend that store items used for a  mock store-front that uses a different JavaScript Framework. 
--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,92 +1,85 @@
 const  app  = require('../server/server');
 const request = require('@code-fellows/supergoose');
-const mockRequest = request(app);
+const serverRequest = request(app);
 
-describe('web server', () => {
+describe('Server testing in 3..2..1', () => {
 
-  it('should respond with a 404 on an invalid route',() => {
-
-    return mockRequest
-      .get('/foobar')
-      .then(results => {
-        expect(results.status).toBe(404);
-      });
-
-  });
-
-  // These tests are wired with async/await --- so much cleaner!
-  it('should respond with a 404 on an invalid method', async () => {
-    const response = await mockRequest.put('/products');
+  test('should respond wtesth a 404 on an invalid route',async() => {
+    const response = await serverRequest.get('/foobar')
     expect(response.status).toBe(404);
   });
 
-  it('can create a record', async () => {
+  // These tests are wired wtesth async/awatest --- so much cleaner!
+  test('should respond wtesth a 404 on an invalid method', async () => {
+    const response = await serverRequest.put('/products');
+    expect(response.status).toBe(404);
+  });
+
+  test('can create a record', async () => {
     const data = {
       item: 'carrots',
       price: .25,
       category: 'Food'
     };
 
-    const response = await mockRequest.post('/products').send(data);
+    const response = await serverRequest.post('/products').send(data);
     expect(response.status).toBe(200);
     expect(response.body._id).toBeDefined();
   });
 
-  it('can get list of records', async () => {
-    const response = await mockRequest.get('/products');
+  test('can get list of records', async () => {
+    const response = await serverRequest.get('/products');
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body.length).toEqual(1);
   });
 
-  it('can get a record', async () => {
+  test('can get a record', async () => {
     const data = {
       item: 'carrots',
       price: .25,
       category: 'Food'
     };
 
-    const getRecord = await mockRequest.post('/products').send(data);
+    const getRecord = await serverRequest.post('/products').send(data);
     let id = getRecord.body._id;
-    const response = await mockRequest.get(`/products/${id}`);
+    const response = await serverRequest.get(`/products/${id}`);
     expect(response.status).toBe(200);
     expect(typeof response.body).toEqual('object');
     expect(response.body._id).toEqual(id);
   });
 
-  it('can update a record', async () => {
+  test('can update a record', async () => {
     const data = {
       item: 'carrots',
       price: .25,
       category: 'Food'
     };
 
-    const getRecord = await mockRequest.post('/products').send(data);
+    const getRecord = await serverRequest.post('/products').send(data);
     let id = getRecord.body._id;
-    const newData = { item: "Broccoli" };
-    const response = await mockRequest.put(`/products/${id}`).send(newData);
+    const newData = {item: "Broccoli" };
+    const response = await serverRequest.put(`/products/${id}`).send(newData);
     expect(response.status).toBe(200);
     expect(typeof response.body).toEqual('object');
     expect(response.body._id).toEqual(id);
     expect(response.body.item).toEqual("Broccoli");
   });
 
-  it('can delete a record', async () => {
+  test('can delete a record', async () => {
     const data = {
       item: 'carrots',
       price: .25,
       category: 'Food'
     };
 
-    const getRecord = await mockRequest.post('/products').send(data);
+    const getRecord = await serverRequest.post('/products').send(data);
     let id = getRecord.body._id;
-    const response = await mockRequest.delete(`/products/${id}`);
+    const response = await serverRequest.delete(`/products/${id}`);
     expect(response.status).toBe(200);
     expect(response.body).toBe(response.body);
 
-    const getResponse = await mockRequest.get('/products');
+    const getResponse = await serverRequest.get('/products');
     expect(getResponse.body.length).toEqual(3);
-
   });
-
 });
